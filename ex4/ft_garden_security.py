5e69b8269baba8e3977995098e8637f08cb2ab5a
Daichi Nakamoto <nakamotodaich@icloud.com>
2026-06-16 18:17:27 +0900

    ex4update

--- ex4/ft_garden_security.py
+++ ex4/ft_garden_security.py
@@ -1,8 +1,8 @@
 class Plant:
     def __init__(self, name, height, age):
-        self.name = name,
-        self._height = height,
-        self._age = age,
+        self.name = name
+        self._height = height
+        self._age = age
 
     def get_heigh(self) -> float:
         return self._height
@@ -10,17 +10,32 @@ class Plant:
     def get_age(self) -> int:
         return self._age
     
-    def set_heigh(self,tole) -> None:
-        if tole < 0:
-            self._tole = tole
+    def set_heigh(self,height) -> None:
+        if height < 0:
+            print(f"{self.name}: Error, height can't be negative Height update rejected")
         else:
-            self._tole = tole
-            print("height can't be negative Height update rejected Rose: Error")
+            self._height = height
+            print(f"Height updated: {self._height}cm")
     
     def set_age(self,age) -> None:
         if age < 0:
-            print(f"")
+            print(f"{self.name}: Error, age can't be negative Age update rejected")
         else:
             self._age = age
-            print(f"age can't be negative Age update rejected Current state:${self.name}: ${self._height}cm, ${self._age} days old")
+            print(f"Age update: {self._age}days")
 
+    def show(self) -> None:
+        print(f"{self.name}: {self._height}cm, {self._age}days old", end="")
+
+
+if __name__ == "__main__":
+    p = Plant("Rose", 15, 10)
+    print(" === Garden Security System ==== Plant created: ")
+    print("Plant created: ", end="")
+    p.show()
+    print("")
+    p.set_heigh(25)
+    p.set_age(30)
+    p.set_heigh(-1)
+    p.set_age(-1)
+    p.show()
